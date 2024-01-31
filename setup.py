420414472f0bdff22c8ae73ee559c34460294ea3
Nicola Cortinovis <nicolacortinovis98@gmail.com>
2024-01-31 16:14:03 +0100

    Fixed some comments in

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 import setuptools
 
-
 # Save the long description from the README.md file
 
 with open("README.md","r", encoding= "utf-8") as f:
     long_description = f.read()
-    
-# First version of the package
+
+# Version of the package
 
 __version__ = "0.0.0"
 
@@ -16,7 +15,7 @@ AUTHOR_USER_NAME = "NicolaCortinovis, Erionis, annalisapaladino, lucapernice"
 AUTHOR = "Cortinovis Nicola, Islamay Erion, Paladino Annalisa, Pernice Luca"
 AUTHOR_EMAIL = "nicolacortinovis98@gmail.com" # to be filled
 
-# Setting up the package
+# Setting up the package, contains the metadata for the package
 
 setuptools.setup(
     name = SRC_REPO,                                                
@@ -26,10 +25,10 @@ setuptools.setup(
     description = "Package for the the conversation summarizer app",
     long_description = long_description,
     long_description_content_type = "text/markdown",
-    url = f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
-    project_urls = {
+    url = f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}", # package repo url
+    project_urls = { # additional urls
         "Bug tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
-    package_dir={"": "src"},
-    packages = setuptools.find_packages(where="src"),
+    package_dir={"": "src"},                            # the root directory of the package
+    packages = setuptools.find_packages(where="src"),   # the list of packages to be included is inside the src folder
 )
