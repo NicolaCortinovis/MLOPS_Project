5971e42352ca4a2854225a3b03803bfbbc31cca5
Nicola Cortinovis <nicolacortinovis98@gmail.com>
2024-01-31 23:36:28 +0100

    Added 03_data_transforamtion.ipynb

--- src/ConversationSummarizer/constants/__init__.py
+++ src/ConversationSummarizer/constants/__init__.py
@@ -5,3 +5,9 @@ CONFIG_FILE_PATH = Path("config/config.yaml")
 
 # Define the path to the parameters file
 PARAMS_FILE_PATH = Path("params.yaml")
+
+# Maximum length for the input sequences. Any input sequence longer than this will be truncated.
+MAX_INPUT_LENGTH = 1024
+
+# Maximum length for the target sequences. Any target sequence longer than this will be truncated.
+MAX_TARGET_LENGTH = 128
