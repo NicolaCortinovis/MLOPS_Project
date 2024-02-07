53f1391eb262679a3f91e324468f09d71b2b8de2
Nicola Cortinovis <nicolacortinovis98@gmail.com>
2024-02-07 15:57:44 +0100

    testing secrets

--- streamlit_app/streamlit_app.py
+++ streamlit_app/streamlit_app.py
@@ -4,57 +4,61 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
+HUGGINGFACE_API_KEY = os.environ["HF_API"]
+
 API_URL = "https://api-inference.huggingface.co/models/Nicovis/ConvSum"
-headers = {"Authorization": "Bearer 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"} # Need to hide this
+headers = {"Authorization": "Bearer " + HUGGINGFACE_API_KEY} # Need to hide this
 
-def query(payload):
-    response = requests.post(API_URL, headers=headers, json=payload)
-    return response.json()
+print(HUGGINGFACE_API_KEY)
+
+# def query(payload):
+#     response = requests.post(API_URL, headers=headers, json=payload)
+#     return response.json()
 
 
 
-def main():
-    st.title('Chat Summarizer')
-    st.sidebar.markdown("""
-    ### How to use the Chat Summarizer :speech_balloon:
+# def main():
+#     st.title('Chat Summarizer')
+#     st.sidebar.markdown("""
+#     How to use the Chat Summarizer :speech_balloon:
 
-    1. **Choose your input method** :point_down:
-        - *Paste text directly* into the text area
-        - *Upload a text file* with your chat content
-    2. **Enter or upload your text** :clipboard:
-        - If you chose to paste the text, enter it in the text area below
-        - If you chose to upload a file, click on 'Browse files' and select the text file from your device
-    3. **Click 'Summarize'** :rocket:
-        - Get a concise summary of your text
-    """)
+#     1. **Choose your input method** :point_down:
+#         - *Paste text directly* into the text area
+#         - *Upload a text file* with your chat content
+#     2. **Enter or upload your text** :clipboard:
+#         - If you chose to paste the text, enter it in the text area below
+#         - If you chose to upload a file, click on 'Browse files' and select the text file from your device
+#     3. **Click 'Summarize'** :rocket:
+#         - Get a concise summary of your text
+#     """)
 
-    upload_option = st.selectbox('How would you like to provide the text?', ('Paste text', 'Upload file'))
-    text = ''
-    if upload_option == 'Paste text':
-        text = st.text_area('Enter your text here')
-    else:
-        uploaded_file = st.file_uploader("Choose a text file", type="txt")
-        if uploaded_file is not None:
-            text = uploaded_file.read().decode()
+#     upload_option = st.selectbox('How would you like to provide the text?', ('Paste text', 'Upload file'))
+#     text = ''
+#     if upload_option == 'Paste text':
+#         text = st.text_area('Enter your text here')
+#     else:
+#         uploaded_file = st.file_uploader("Choose a text file", type="txt")
+#         if uploaded_file is not None:
+#             text = uploaded_file.read().decode()
 
-    if st.button('Summarize'):
-        if text:
-            with st.spinner('Summarizing...'):
-                try:
-                    output = query({
-                        "inputs": text,
-                        "options": {
-                            "wait_for_model": True
-                        }
-                    })
-                    summary = output[0]['generated_text']
-                    st.subheader('Summary')
-                    st.write(summary)
+#     if st.button('Summarize'):
+#         if text:
+#             with st.spinner('Summarizing...'):
+#                 try:
+#                     output = query({
+#                         "inputs": text,
+#                         "options": {
+#                             "wait_for_model": True
+#                         }
+#                     })
+#                     summary = output[0]['generated_text']
+#                     st.subheader('Summary')
+#                     st.write(summary)
 
-                except:
-                    st.error('Error: Something went wrong. Please try again later.')
-        else:
-            st.error('Error: Please enter some text or upload a file before summarizing.')
+#                 except:
+#                     st.error('Error: Something went wrong. Please try again later.')
+#         else:
+#             st.error('Error: Please enter some text or upload a file before summarizing.')
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
